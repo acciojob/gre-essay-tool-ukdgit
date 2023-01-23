44603d907542d5eaa8eef2487c10cc96e1846b7e
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-01-23 13:46:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your code here
+function count_word(val) {
+  var wom = val.match(/\S+/g);
+  return {
+    words: wom ? wom.length : 0,
+  };
+}
+var textContent = document.getElementById("evaluatedText");
+var showWordCount = document.getElementById("wordCount");
+
+textContent.addEventListener(
+  "input",
+  function () {
+    var v = count_word(this.value);
+    showWordCount.innerHTML = v.words;
+  },
+  false
+);
